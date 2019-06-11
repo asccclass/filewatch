8db8f10913e0a4f3943dbf5e33e167bec5191c1e
Chih-Han Liu <andyliu@sinica.edu.tw>
2019-06-11 15:11:28 +0800

    <文件修改>: 將filepool改為tmp

--- fwatcher.go
+++ fwatcher.go
@@ -2,12 +2,11 @@ package main
 
 import (
    "log"
-   // "github.com/howeyc/fsnotify"
    "github.com/asccclass/filewatch/libs/filemonitor"
 )
 
 func main() {
-   M, err := filemonitor.NewMonitor("./filepool")
+   M, err := filemonitor.NewMonitor("./tmp")
    if err != nil {
       log.Println(err)
       return
