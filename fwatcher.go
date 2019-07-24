87f5a1bf9e668a8f0a6bb2dbc123f647054cbb08
Chih-Han Liu <andyliu@sinica.edu.tw>
2019-07-24 16:41:21 +0800

    <文件修改>: 備份

--- fwatcher.go
+++ fwatcher.go
@@ -1,19 +1,31 @@
 package main
 
 import (
-
+   "os"
    log "github.com/sirupsen/logrus"
    "github.com/asccclass/filewatch/libs/filemonitor"
 )
 
 func main() {
-   nightswatch, err := filemonitor.NewMonitor("./tmp", "")
+   dir, err := os.Getwd()
+   if err != nil {
+      log.Printf("Initial error:%v.", err)
+   }
+
+   documentRoot := os.Getenv("DocumentRoot")
+   if documentRoot == "" {
+      documentRoot = "www"
+   }
+   documentRoot = dir + "/" + documentRoot
+
+   nightswatch, err := filemonitor.NewMonitor(documentRoot, "")
    if err != nil {
       log.Println(err)
       return
    }
    log.Println("Night gathers, and now my watch begins,It shall not end until my death....")
+   log.Printf("I'm watching %v....", documentRoot)
    nightswatch.Do()
-   nightswatch.FileWatcher.Watch("./filepool")
+   nightswatch.FileWatcher.Watch(documentRoot)
    select {}
 }
